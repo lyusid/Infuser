5911c3af9b07a00bcf7bf8166c1db0b3160f437a
吕鑫涛 <lvxintao@dayugame.net>
2017-10-30 14:39:02 +0800

    MODIFY JAVA LIBRARY AS ANDROID LIBRARY

---
+++ infuser/src/main/java/com/lxt/infuser/Binder.java
@@ -0,0 +1,21 @@
+package com.lxt.infuser;
+
+import android.support.annotation.UiThread;
+
+/**
+ * @author lxt <lxt352@gmail.com>
+ * @since 2017/10/30.
+ */
+
+public interface Binder {
+
+    @UiThread
+    void bind();
+
+    Binder BINDER_EMPTY = new Binder() {
+        @Override
+        public void bind() {
+
+        }
+    };
+}
--- infuser/src/main/java/com/lxt/infuser/Infuser.java
+++ infuser/src/main/java/com/lxt/infuser/Infuser.java
@@ -1,11 +1,38 @@
 package com.lxt.infuser;
 
-import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+import android.support.annotation.UiThread;
+import android.util.Log;
+
+import java.lang.reflect.Constructor;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Infuser {
 
-    @MainThread
-    public static void bind(Object object) {
+    private static final String TAG = Infuser.class.getSimpleName();
 
+    private static final Map<Class<?>, Constructor<? extends Binder>> BINDERS = new LinkedHashMap<>();
+
+    @NonNull
+    @UiThread
+    public static Binder bind(Object object) {
+        return createBinder(object);
+    }
+
+    private static Binder createBinder(Object object) {
+        Class<?> clazz = object.getClass();
+        Constructor<? extends Binder> constructor = findBinderConstructorForClass(clazz);
+        if (constructor == null)
+            return Binder.BINDER_EMPTY;
+        return null;
+    }
+
+    private static Constructor<? extends Binder> findBinderConstructorForClass(Class<?> clazz) {
+        Constructor<? extends Binder> constructor = BINDERS.get(clazz);
+        if (constructor != null) {
+            Log.d(TAG,"Cached binder in binding map.");
+        }
+        return null;
     }
 }
