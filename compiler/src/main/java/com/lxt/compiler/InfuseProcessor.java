cdabb1ed07c68846e84efd199b6b024650261742
吕鑫涛 <lxt352@gmail.com>
2017-10-31 11:49:02 +0800

    REMOVE UNUSED CODES

--- compiler/src/main/java/com/lxt/compiler/InfuseProcessor.java
+++ compiler/src/main/java/com/lxt/compiler/InfuseProcessor.java
@@ -3,11 +3,7 @@ package com.lxt.compiler;
 import com.google.auto.common.SuperficialValidation;
 import com.google.auto.service.AutoService;
 import com.lxt.annotation.Infuse;
-import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.MethodSpec.Builder;
-import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 
 import java.io.IOException;
@@ -29,22 +25,13 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
-import javax.lang.model.element.Name;
-import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
 import javax.tools.Diagnostic.Kind;
 
 @AutoService(Processor.class)
 public class InfuseProcessor extends AbstractProcessor {
-
-    private static final String PACKAGE_NAME = "com.org.lxt.infuse";
-
-    private static final String METHOD_NEWINSTANCE = "build";
-
-    private static final String PARAMA_OBJECT = "object";
 
     private Elements elements;
 
@@ -132,89 +119,6 @@ public class InfuseProcessor extends AbstractProcessor {
         enclosingElements.add(enclosingElement);
         if (!element.getModifiers().contains(Modifier.PUBLIC))
             ModifierException.printException(element.getSimpleName().toString(), enclosingElement.getQualifiedName().toString());
-    }
-
-    private Map<TypeElement, ConstructorBinder> findAndParseElement(RoundEnvironment roundEnvironment) {
-        for (Element element : roundEnvironment.getElementsAnnotatedWith(Infuse.class)) {
-            if (!SuperficialValidation.validateElement(element))
-                continue;
-            PackageElement packageElement = elements.getPackageOf(element);
-            Name qualifiedName = packageElement.getQualifiedName();
-            printNote("Find a element annotated with Infuse " + qualifiedName);
-            boolean isField = element.getKind() == ElementKind.FIELD;
-            if (isField)
-//                process(element);
-//            else
-                printError("The type of target should be Filed");
-        }
-        return null;
-    }
-
-    private void process(Element element) {
-        VariableElement typeElement = (VariableElement) element;
-        String simpleName = typeElement.getSimpleName().toString();
-        TypeName typeName = TypeName.get(typeElement.asType());
-        Element enclosingElement = typeElement.getEnclosingElement();
-        String typeMirrorName = enclosingElement.asType().toString();
-        printNote("Enclosing Element Name = " + typeMirrorName);
-        if (classFile.contains(typeMirrorName)) {
-            TypeSpec.Builder builder = typeSpecMap.get(typeMirrorName);
-            Builder factory = MethodSpec.methodBuilder(simpleName)
-                    .addModifiers(Modifier.PUBLIC, Modifier.SYNCHRONIZED, Modifier.STATIC)
-                    .addParameter(ClassName.get(enclosingElement.asType()), PARAMA_OBJECT)
-                    .returns(typeName);
-            Set<Modifier> modifiers = typeElement.getModifiers();
-            Name objectName = typeElement.getSimpleName();
-            if (modifiers.contains(Modifier.PUBLIC)) {
-                factory.addStatement("$N.$N = ($T)new $T()", PARAMA_OBJECT, objectName,
-                        ClassName.get(typeElement.asType()), typeName)
-                        .addStatement("return $N.$N", PARAMA_OBJECT, typeElement.getSimpleName());
-            } else {
-                factory.addStatement("return null");
-            }
-            TypeSpec typeSpec = builder.addMethod(factory.build())
-                    .build();
-            JavaFile javaFile = JavaFile.builder(PACKAGE_NAME, typeSpec)
-                    .build();
-            try {
-                javaFile.writeTo(System.out);
-                javaFile.writeTo(filer);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return;
-        }
-        classFile.add(typeMirrorName);
-        TypeSpec.Builder typeSpecBuilder = TypeSpec.classBuilder("Infuser")
-                .addModifiers(Modifier.PUBLIC);
-        JavaFile javaFile;
-        printNote(typeElement.getSimpleName() + " " + typeName);
-        Builder factory = MethodSpec.methodBuilder(simpleName)
-                .addModifiers(Modifier.PUBLIC, Modifier.SYNCHRONIZED, Modifier.STATIC)
-                .addParameter(ClassName.get(enclosingElement.asType()), "object")
-                .returns(typeName);
-        typeSpecMap.put(typeMirrorName, typeSpecBuilder);
-        Set<Modifier> modifiers = typeElement.getModifiers();
-        Name objectName = typeElement.getSimpleName();
-        if (modifiers.contains(Modifier.PUBLIC)) {
-            factory.addStatement("$N.$N = ($T)new $T()", PARAMA_OBJECT, objectName,
-                    ClassName.get(typeElement.asType()), typeName)
-                    .addStatement("return $N.$N", PARAMA_OBJECT, typeElement.getSimpleName());
-        } else {
-//            printError("This filed should be public");
-            factory.addStatement("return null");
-        }
-        TypeSpec typeSpec = typeSpecBuilder.addMethod(factory.build())
-                .superclass(TypeName.get(enclosingElement.asType()))
-                .build();
-        javaFile = JavaFile.builder(PACKAGE_NAME, typeSpec)
-                .build();
-        try {
-            javaFile.writeTo(System.out);
-            javaFile.writeTo(filer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     private void printNote(String message) {
