08ee376464b976a016b912c0c509f7288958be7b
吕鑫涛 <lxt352@gmail.com>
2017-11-01 11:12:26 +0800

    ADD SIMPLE IN INNER CLASS

--- app/src/main/java/com/lxt/infuser/MainActivity.java
+++ app/src/main/java/com/lxt/infuser/MainActivity.java
@@ -45,9 +45,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Infuser.bind(this);
+        new InnerClass();
     }
 
-    public static class InnerClass {
+    class InnerClass {
 
         @InfuseString({"Ivy", "girl"})
         public Singer singer9;
